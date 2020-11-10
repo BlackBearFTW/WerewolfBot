318faf0740d0b4e667d735d4e75615f0591971a8
BlackBearFTW <robinmager@home.nl>
2020-11-10 20:36:21 +0100

    Update start.js

---
+++ src/commands/start.js
@@ -0,0 +1,16 @@
+const Match = require('../classes/Match');
+module.exports = {
+    name: 'start',
+    execute: async(message, args) => {
+            await Match.getUsers()
+
+            /* TODO
+             * Update match "STARTED" to 1
+             *
+             * 
+             * 
+             * 
+             * 
+             */
+    },
+};
