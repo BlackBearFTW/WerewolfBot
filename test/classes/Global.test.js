02598a0db6b9392444887656ca0ec8d917647f0c
BlackBearFTW <robinmager@home.nl>
2020-11-26 14:18:40 +0100

    added test for Global class

--- test/classes/Global.test.js
+++ test/classes/Global.test.js
@@ -1,9 +1,8 @@
 const {BaseMocks, CustomMocks } = require('@lambocreeper/mock-discord.js');
 const Global = require('../../src/classes/Global');
 
-describe('Global class', function() {
-    it('throwError method', function() {
-        // expect(Global.throwError(BaseMocks.getMessage(), 'Error message')).toBe(true);
-        expect(Global.throwError(1, 1)).toBe(2)
+describe('Global class', () => {
+    it('throwError method', async() => {
+        await expect(Global.throwError(BaseMocks.getMessage(), 'Error message')).resolves.not.toThrow();
     });
 });
