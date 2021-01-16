0cfbcdd3ce90ce70948d3e692c6c7f136f9ce18e
BlackBearFTW <robinmager@home.nl>
2021-01-16 22:19:34 +0100

    removed build folder from remote and local

--- build/commands/other/ping.js
+++
@@ -1,7 +0,0 @@
-import { client } from "../../index.js";
-export const command = {
-    name: 'ping',
-    execute(message, args) {
-        message.channel.send(`Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-    }
-};
