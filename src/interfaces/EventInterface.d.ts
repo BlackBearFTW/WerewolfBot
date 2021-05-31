624ef3c685632a9f85df1adf55872f2d252c93e2
BlackBearFTW <robinmager@home.nl>
2021-05-31 20:20:48 +0200

    Removed interfaces

--- src/interfaces/EventInterface.d.ts
+++
@@ -1,9 +0,0 @@
-import {ClientEvents} from "discord.js";
-
-interface EventInterface {
-    event: keyof ClientEvents;
-    once: boolean;
-    disabled: boolean;
-    // @ts-ignore
-    execute(...parameters?: any[]): void;
-}
